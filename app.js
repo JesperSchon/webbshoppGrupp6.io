54dec4d71a9fe35fc7a541ee5a4509d861ed8474
JesperSchon <119195812+JesperSchon@users.noreply.github.com>
2023-03-26 15:31:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -18,21 +18,38 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Products fetched:", products);
         products.forEach((product) => {
           const productCard = `
-<div class="col-md-4">
-  <div class="card mb-4">
-    <img src="${product.image}" class="card-img-top product-image" alt="${product.title}">
-    <div class="card-body">
-      <h5 class="card-title">${product.title}</h5>
-      <p class="card-text">${product.description}</p>
-      <p class="card-text">$${product.price}</p>
-      <button class="btn btn-primary order-button" data-product-id="${product.id}">Order now</button>
-    </div>
-  </div>
-</div>`;
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img src="${product.image}" class="card-img-top product-image" alt="${product.title}">
+              <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text short-description">${product.description}</p>
+                <button class="btn btn-link read-more">Read More</button>
+                <p class="card-text">$${product.price}</p>
+                <button class="btn btn-primary order-button" data-product-id="${product.id}">Order now</button>
+              </div>
+            </div>
+          </div>`;
+          
           // ... existing code ...
 
           productsContainer.innerHTML += productCard;
         });
+
+        const readMoreButtons = document.querySelectorAll(".read-more");
+readMoreButtons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    const shortDescription = event.target.previousElementSibling;
+    shortDescription.classList.toggle("short-description");
+
+    if (event.target.textContent === "Read More") {
+      event.target.textContent = "Read Less";
+    } else {
+      event.target.textContent = "Read More";
+    }
+  });
+});
+
 
         const orderButtons = document.querySelectorAll(".order-button");
         orderButtons.forEach((button) => {
@@ -114,3 +131,4 @@ document.addEventListener("DOMContentLoaded", function () {
     orderIDField.innerHTML = orderID;
   }
 });
+
